280e09f4b43dbee5e4bca479c900bbd5dce09a55
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-09-11 14:45:25 +0100

    react calculator

--- src/App.js
+++ src/App.js
@@ -2,25 +2,61 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    display: "",
+    numA:"",
+    numB:"",
+  };
+
+inputHandler=(input)=>{
+  const newNum=this.state.numB+input;
+  const disNum = this.state.display+input
+  this.setState({numB:newNum,display:disNum})
+}
+
+clearHandler=()=>{
+  this.setState({numA:"",numB:"",display:""})
+}
+
+operatorHandler=(input)=>{
+  const newNum=this.state.numB+input;
+  this.setState({numB:newNum, display:""})
+}
+
+equalHandler=()=>{
+  const newNum=eval(this.state.numB)
+  this.setState({numB:newNum, display:newNum})
+}
+
+  render() {
+    return (
+      <div className="calculator">
+        <div id="display">
+          {this.state.display}
+        </div>
+        <div id="calc-btns">
+          <button className="white" onClick={() => this.inputHandler("7")}>7</button>
+          <button className="white" onClick={() => this.inputHandler("8")}>8</button>
+          <button className="white" onClick={() => this.inputHandler("9")}>9</button>
+          <button className="red" onClick={()=>this.operatorHandler("/")}>÷</button>
+          <button className="white" onClick={() => this.inputHandler("4")}>4</button>
+          <button className="white" onClick={() => this.inputHandler("5")}>5</button>
+          <button className="white" onClick={() => this.inputHandler("6")}>6</button>
+          <button className="red" onClick={()=>this.operatorHandler("*")}>x</button>
+          <button className="white" onClick={() => this.inputHandler("1")}>1</button>
+          <button className="white" onClick={() => this.inputHandler("2")}>2</button>
+          <button className="white" onClick={() => this.inputHandler("3")}>3</button>
+          <button className="red" onClick={()=>this.operatorHandler("-")}>-</button>
+          <button className="white" onClick={this.clearHandler}>clear</button>
+          <button className="white" onClick={() => this.inputHandler("0")}>0</button>
+          <button className="red" onClick={()=>this.operatorHandler("+")}>+</button>
+          <button className="red" onClick={this.equalHandler}>=</button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
