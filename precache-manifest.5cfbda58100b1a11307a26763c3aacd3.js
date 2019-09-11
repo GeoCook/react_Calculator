2b8330077a9f29accb52edf1d921be07e68da65f
CN74 <cn74@CN74s-MacBook-Pro.local>
2019-09-11 15:00:58 +0100

    Updates

---
+++ precache-manifest.5cfbda58100b1a11307a26763c3aacd3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444e9f4dd3298372d262062fbe78496f",
+    "url": "/react_calculator/index.html"
+  },
+  {
+    "revision": "747a1d91efcf7ce646c0",
+    "url": "/react_calculator/static/css/main.a2082786.chunk.css"
+  },
+  {
+    "revision": "fde96a308598f6d414c6",
+    "url": "/react_calculator/static/js/2.08ff8035.chunk.js"
+  },
+  {
+    "revision": "747a1d91efcf7ce646c0",
+    "url": "/react_calculator/static/js/main.03d55be6.chunk.js"
+  },
+  {
+    "revision": "d0377e40eb737f10fea1",
+    "url": "/react_calculator/static/js/runtime~main.aeddf56a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
